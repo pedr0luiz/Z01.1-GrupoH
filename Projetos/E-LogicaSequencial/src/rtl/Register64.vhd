bb638c0a910cf64cd16c89916dfefc8674edeef4
gabrielzezze <gabrielzezze@me.com>
2019-03-25 19:55:20 -0300

    Register64 testado e Ok

--- Projetos/E-LogicaSequencial/src/rtl/Register64.vhd
+++ Projetos/E-LogicaSequencial/src/rtl/Register64.vhd
@@ -1,6 +1,6 @@
 -- Elementos de Sistemas
 -- by Luciano Soares
--- Register64.vhd
+-- Register32.vhd
 
 Library ieee;
 use ieee.std_logic_1164.all;
@@ -18,8 +18,30 @@ architecture arch of Register64 is
   -- Aqui declaramos sinais (fios auxiliares)
   -- e componentes (outros módulos) que serao
   -- utilizados nesse modulo.
+component Register32 is
+port(
+		clock:   in STD_LOGIC;
+		input:   in STD_LOGIC_VECTOR(31 downto 0);
+		load:    in STD_LOGIC;
+		output: out STD_LOGIC_VECTOR(31 downto 0)
+	);
+end component;
+
+signal signal1, signal2 : STD_LOGIC_VECTOR(31 downto 0);
 
 begin
+	bite1: Register32 port map(
+		clock  => clock,
+		input  => input(31 downto 0),
+		load   => load,
+		output => signal1);
 
+	bite2: Register32 port map(
+		clock    => clock,
+		input => input(63 downto 32),
+		load => load,
+		output => signal2);		
 
+	output <= signal2 & signal1;
+	
 end architecture;
--- Projetos/E-LogicaSequencial/vunit_out/test_output/lib.tb_register8.all_0308869cb9dc72b51c624fc073592f3339b2d81c/modelsim/batch.do
+++ Projetos/E-LogicaSequencial/vunit_out/test_output/lib.tb_register8.all_0308869cb9dc72b51c624fc073592f3339b2d81c/modelsim/batch.do
@@ -1,5 +1,5 @@
 onerror {quit -code 1}
-source "/home/borg/Desktop/Insper/Z01.1-Hardware/Projetos/E-LogicaSequencial/vunit_out/test_output/lib.tb_register8.all_0308869cb9dc72b51c624fc073592f3339b2d81c/modelsim/common.do"
+source "/home/gabrielzezze/Z01.1-Hardware/Projetos/E-LogicaSequencial/vunit_out/test_output/lib.tb_register8.all_0308869cb9dc72b51c624fc073592f3339b2d81c/modelsim/common.do"
 set failed [vunit_load]
 if {$failed} {quit -code 1}
 set failed [vunit_run]
