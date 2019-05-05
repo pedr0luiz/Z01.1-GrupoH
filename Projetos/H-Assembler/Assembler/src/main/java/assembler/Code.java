4ebb18482f5aa8f9cff64d4b138745da2d9a67eb
Thomas Queiroz <thomasqueirozb@gmail.com>
2019-05-05 17:15:44 -0300

    Dest testado e funcionando

--- Projetos/H-Assembler/Assembler/src/main/java/assembler/Code.java
+++ Projetos/H-Assembler/Assembler/src/main/java/assembler/Code.java
@@ -16,9 +16,54 @@ public class Code {
      * @return Opcode (String de 4 bits) com código em linguagem de máquina para a instrução.
      */
     public static String dest(String[] mnemnonic) {
+        String joined = "";
 
+        for (int idx = 2; idx < mnemnonic.length; idx++){
+            String str = mnemnonic[idx];
+            int str_len = str.length();
+            for (int char_idx = 0; char_idx < str_len ; char_idx++){
+                char temp_char = str.charAt(idx);
+                if (temp_char != '%'){
+                    joined += temp_char;
+                }
+            }
+        }
 
-        return "";
+        switch (joined) {
+            case "A":
+                return "1000";
+            case "D":
+                return "0010";
+            case "S":
+                return "0100";
+            case "(A)":
+                return "0001";
+            case "D(A)":
+                return "0011";
+            case "S(A)":
+                return "0101";
+            case "SD":
+                return "0110";
+            case "SD(A)":
+                return "0111";
+            case "A(A)":
+                return "1001";
+            case "AD":
+                return "1010";
+            case "AD(A)":
+                return "1011";
+            case "AS":
+                return "1100";
+            case "AS(A)":
+                return "1101";
+            case "ASD":
+                return "1110";
+            case "ASD(A)":
+                return "1111";
+            default:
+                return "0000";
+
+        }
     }
 
     /**
